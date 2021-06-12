a2455e1dabc4c5eb783c51fdebcf428c9b95b672
unknown <roselleannguzman14@gmail.com>
2021-06-12 15:52:26 +0800

    Latest Changes as of June 12 2021

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -77,9 +77,11 @@ $app->configure('app');
 // $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
+
 $app->middleware([
     App\Http\Middleware\AuthenticateAccess::class,
 ]);
+
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
